c857e3bc94466cd0f8cb3a4c4929ed948d2324f1
Capeudinho <archmagepyromancer@gmail.com>
2024-08-23 17:19:06 -0300

    Criação de model de event, task e category e atualização de model de user

---
+++ app/Models/Event.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Event extends Model
+{
+	use HasFactory;
+
+	protected $fillable = [
+		'title',
+		'description',
+		'start',
+		'category_id',
+		'user_id',
+	];
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function category(): BelongsTo
+	{
+		return $this->belongsTo(Category::class);
+	}
+
+	public function task(): HasOne
+	{
+		return $this->hasOne(Task::class);
+	}
+}
