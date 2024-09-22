3acdba2c1f0ed86ab48f3377954d2c243629e33d
Laysa Protásio <117881941+laysaprotasio@users.noreply.github.com>
2024-09-22 15:28:26 -0300

    View de categorias

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@push('styles')
+    <link rel="stylesheet" href="/css/index.css">
+@endpush
+
+@section('content')
+    <div class="p-6">
+        <div class="flex justify-between items-center mb-4">
+            <h1 class="text-2xl">Suas Categorias</h1>
+            <a href="{{ route('category.create') }}" class="add-button">
+                <img src="{{ asset('icons/plus.svg') }}" alt="Add Icon"> Criar Nova Categoria
+            </a>
+        </div>
+
+    </div>
+
+    @include('components.table', [
+        'header' => ['ID', 'Nome', 'Cor'],
+        'content' => $categories->map(function ($category) {
+            $colorTranslations = [
+                '#CC0000' => 'Vermelho',
+                '#CC6600' => 'Laranja',
+                '#CCCC00' => 'Amarelo',
+                '#66CC00' => 'Lima',
+                '#00CC00' => 'Verde',
+                '#00CC66' => 'Menta',
+                '#00CCCC' => 'Ciano',
+                '#0066CC' => 'Azul 1',
+                '#0000CC' => 'Azul',
+                '#6600CC' => 'Roxo',
+                '#CC00CC' => 'Magenta',
+                '#CC0066' => 'Rosa',
+            ];
+    
+            return [
+            $category->id,
+            ['name' => $category->name, 'color' => $category->color],
+            ['name' => $colorTranslations[$category->color] ?? $category->color, 'color' => $category->color],
+        ];
+        }),
+        'editRoute' => 'category.edit',
+        'deleteRoute' => 'category.destroy',
+    ])
+@endsection
+
+@push('scripts')
+    <script src="/js/searchtable.js"></script>
+@endpush
