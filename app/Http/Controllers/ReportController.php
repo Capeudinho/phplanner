35603f3f04cc1f9897dad958028aba2b226d3aef
Capeudinho <archmagepyromancer@gmail.com>
2024-09-23 15:30:00 -0300

    Refatoração das consultas

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,161 +11,213 @@ class ReportController extends Controller
 	public function index()
 	{
 		$id = Auth::id();
-		$reports = [
-			'finishedGoals' => [
+
+		foreach (['7', '30', '365'] as $interval) {
+
+			$reports[$interval]['finishedGoals'] = [
 				'title' => 'Quantidade e porcentagem de metas cumpridas',
-				'aliases' => ['Quantidade', "Porcentagem"],
+				'aliases' => ['Quantidade', 'Porcentagem'],
 				'results' => [],
-			],
-			'finishedTasks' => [
-				'title' => 'Quantidade e porcentagem de tarefas finalizadas',
-				'aliases' => ['Quantidade', "Porcentagem"],
-				'results' => [],
-			],
-			'productiveMonths' => [
-				'title' => 'Meses mais produtivos (em ordem)',
-				'aliases' => ['Mês', 'Quantidade'],
-				'results' => [],
-			],
-			'productiveWeeks' => [
-				'title' => 'Semanas mais produtivas (em ordem)',
-				'aliases' => ['Mês', 'Semana', 'Quantidade'],
-				'results' => [],
-			],
-			'productiveTurns' => [
-				'title' => 'Turnos mais produtivos (em ordem)',
-				'aliases' => ['Turno', 'Quantidade'],
-				'results' => [],
-			],
-			'finishedTaskCategories' => [
-				'title' => 'Categorias de tarefas mais realizadas (em ordem)',
-				'aliases' => ['Categoria', 'Quantidade'],
-				'results' => [],
-			],
-			'finishedGoalCategories' => [
-				'title' => 'Categorias de metas mais realizadas (em ordem)',
-				'aliases' => ['Categoria', 'Quantidade'],
-				'results' => [],
-			],
-		];
+			];
 
-		$reports['finishedGoals']['results'] = DB::select(
-			'SELECT COUNT(*) AS quantity, (CAST(COUNT(*) AS FLOAT)/(
-				SELECT COUNT(*)
+			$reports[$interval]['finishedGoals']['results'] = DB::select(
+				'SELECT COUNT(*) AS quantity, CASE 
+					WHEN (
+						SELECT COUNT(*)
+						FROM users u, events e, goals g
+						WHERE u.id = '.$id.
+						' AND u.id = e.user_id
+						AND e.id = g.event_id
+						AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+						AND e.start <= NOW()) = 0
+					THEN NULL
+					ELSE (CAST(COUNT(*) AS FLOAT)/(
+						SELECT COUNT(*)
+						FROM users u, events e, goals g
+						WHERE u.id = '.$id.
+						' AND u.id = e.user_id
+						AND e.id = g.event_id
+						AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+						AND e.start <= NOW()
+					))*100
+				END AS percent
 				FROM users u, events e, goals g
-				WHERE u.id = ?
-				AND u.id = e.user_id
+				WHERE u.id = '.$id.
+				' AND u.id = e.user_id
 				AND e.id = g.event_id
-				AND e.start >= NOW()-INTERVAL \'365 DAYS\'
-				AND e.start <= NOW()))*100 AS percent
-			FROM users u, events e, goals g
-			WHERE u.id = ?
-			AND u.id = e.user_id
-			AND e.id = g.event_id
-			AND g.status = \'succeeded\'
-			AND e.start >= NOW()-INTERVAL \'365 DAYS\'
-			AND e.start <= NOW()',
-			[$id, $id]
-		);
+				AND g.status = \'finished\'
+				AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+				AND e.start <= NOW()'
+			);
 
-		$reports['finishedTasks']['results'] = DB::select(
-			'SELECT COUNT(*) AS quantity, (CAST(COUNT(*) AS FLOAT)/(
-				SELECT COUNT(*)
+			$reports[$interval]['finishedTasks'] = [
+				'title' => 'Quantidade e porcentagem de tarefas cumpridas',
+				'aliases' => ['Quantidade', 'Porcentagem'],
+				'results' => [],
+			];
+
+			$reports[$interval]['finishedTasks']['results'] = DB::select(
+				'SELECT COUNT(*) AS quantity, CASE 
+					WHEN (
+						SELECT COUNT(*)
+						FROM users u, events e, tasks t
+						WHERE u.id = '.$id.
+						' AND u.id = e.user_id
+						AND e.id = t.event_id
+						AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+						AND e.start <= NOW()) = 0
+					THEN NULL
+					ELSE (CAST(COUNT(*) AS FLOAT)/(
+						SELECT COUNT(*)
+						FROM users u, events e, tasks t
+						WHERE u.id = '.$id.
+						' AND u.id = e.user_id
+						AND e.id = t.event_id
+						AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+						AND e.start <= NOW()
+					))*100
+				END AS percent
 				FROM users u, events e, tasks t
-				WHERE u.id = ' . $id .
+				WHERE u.id = '.$id.
 				' AND u.id = e.user_id
 				AND e.id = t.event_id
-				AND e.start >= NOW()-INTERVAL \'365 DAYS\'
-				AND e.start <= NOW()))*100 AS percent
-			FROM users u, events e, tasks t
-			WHERE u.id = ?
-			AND u.id = e.user_id
-			AND e.id = t.event_id
-			AND t.status = \'finished\'
-			AND e.start >= NOW()-INTERVAL \'365 DAYS\'
-			AND e.start <= NOW()',
-            [$id]
-		);
-		
-		$reports['productiveMonths']['results'] = DB::select(
-			'SELECT EXTRACT(MONTH FROM e.start) AS month,
-			COUNT(*) AS tasks_count
-			FROM users u, events e, tasks t
-			WHERE u.id = ?
-			AND u.id = e.user_id
-			AND e.id = t.event_id
-			AND t.status = \'finished\'
-			AND e.start >= NOW() - INTERVAL \'365 DAYS\'
-			AND e.start <= NOW()
-			GROUP BY month
-			ORDER BY tasks_count DESC',
-			[$id]
-		);
+				AND t.status = \'finished\'
+				AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+				AND e.start <= NOW()'
+			);
 
-		$reports['productiveWeeks']['results'] = DB::select(
-			'SELECT 
-				EXTRACT(MONTH FROM e.start) AS month,
-       			CEIL(EXTRACT(DAY FROM e.start) / 7) AS week,
-        		COUNT(*) AS tasks_count
-			FROM users u, events e, tasks t
-			WHERE u.id = ?
-			AND u.id = e.user_id
-			AND e.id = t.event_id
-			AND t.status = \'finished\'
-			AND e.start >= NOW() - INTERVAL \'365 DAYS\'
-			AND e.start <= NOW()
-			GROUP BY month, week
-			ORDER BY tasks_count DESC',
-			[$id]
-		);
+			if ($interval == '365') {
 
-		$reports['productiveTurns']['results'] = DB::select(
-			'SELECT CASE
-        		WHEN EXTRACT(HOUR FROM e.start) >= 0 AND EXTRACT(HOUR FROM e.start) < 6 THEN \'Madrugada\'
-        		WHEN EXTRACT(HOUR FROM e.start) >= 6 AND EXTRACT(HOUR FROM e.start) < 12 THEN \'Manhã\'
-        		WHEN EXTRACT(HOUR FROM e.start) >= 12 AND EXTRACT(HOUR FROM e.start) < 18 THEN \'Tarde\'
-        		ELSE \'Noite\'
-    		END AS turn,
-			COUNT(*) AS quantity
-			FROM users u, events e, tasks t
-			WHERE u.id = ?
-			AND u.id = e.user_id
-			AND e.id = t.event_id
-			AND t.status = \'finished\'
-			AND e.start >= NOW()-INTERVAL \'365 DAYS\'
-			AND e.start <= NOW()
-			GROUP BY turn
-			ORDER BY COUNT(*) DESC',
-            [$id]
-		);
+				$reports[$interval]['productiveMonths'] = [
+					'title' => 'Meses mais produtivos',
+					'aliases' => ['Mês', 'Quantidade'],
+					'results' => [],
+				];
+			
+				$reports[$interval]['productiveMonths']['results'] = DB::select(
+					'SELECT EXTRACT(MONTH FROM e.start) AS month,
+					COUNT(*) AS tasks_count
+					FROM users u, events e, tasks t
+					WHERE u.id = '.$id.
+					' AND u.id = e.user_id
+					AND e.id = t.event_id
+					AND t.status = \'finished\'
+					AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+					AND e.start <= NOW()
+					GROUP BY month
+					ORDER BY tasks_count DESC'
+				);
 
-		$reports['finishedTaskCategories']['results'] = DB::select(
-            'SELECT c.name, COUNT(*) AS quantity
-            FROM events e
-            INNER JOIN categories c ON e.category_id = c.id
-            INNER JOIN tasks t ON t.event_id = e.id
-            WHERE e.user_id = ?
-			AND e.start >= NOW()-INTERVAL\'365 DAYS\'
-			AND e.start <= NOW()
-            AND t.status = \'finished\'
-            GROUP BY c.id, c.name
-            ORDER BY quantity DESC',
-            [$id]
-        );
+				$reports[$interval]['productiveWeeks'] = [
+					'title' => 'Semanas mais produtivas',
+					'aliases' => ['Mês', 'Semana', 'Quantidade'],
+					'results' => [],
+				];
 
-		$reports['finishedGoalCategories']['results'] = DB::select(
-            'SELECT c.name,COUNT(*) AS quantity
-            FROM events e
-            INNER JOIN categories c ON e.category_id = c.id
-            INNER JOIN goals g ON g.event_id = e.id
-            WHERE e.user_id = ?
-			AND e.start >= NOW()-INTERVAL \'365 DAYS\'
-			AND e.start <= NOW()
-            AND g.status = \'succeeded\'
-            GROUP BY c.id, c.name
-            ORDER BY quantity DESC',
-            [$id]
-        );
+				$reports[$interval]['productiveWeeks']['results'] = DB::select(
+					'SELECT
+						EXTRACT(MONTH FROM e.start) AS month,
+						CEIL(EXTRACT(DAY FROM e.start) / 7) AS week,
+						COUNT(*) AS tasks_count
+					FROM users u, events e, tasks t
+					WHERE u.id = '.$id.
+					' AND u.id = e.user_id
+					AND e.id = t.event_id
+					AND t.status = \'finished\'
+					AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+					AND e.start <= NOW()
+					GROUP BY month, week
+					ORDER BY tasks_count DESC'
+				);
+
+			} else if ($interval == '30') {
+
+				$reports[$interval]['productiveWeeks'] = [
+					'title' => 'Semanas mais produtivas',
+					'aliases' => ['Semana', 'Quantidade'],
+					'results' => [],
+				];
+
+				$reports[$interval]['productiveWeeks']['results'] = DB::select(
+					'SELECT
+						CEIL(EXTRACT(DAY FROM e.start) / 7) AS week,
+						COUNT(*) AS tasks_count
+					FROM users u, events e, tasks t
+					WHERE u.id = '.$id.
+					' AND u.id = e.user_id
+					AND e.id = t.event_id
+					AND t.status = \'finished\'
+					AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+					AND e.start <= NOW()
+					GROUP BY week
+					ORDER BY tasks_count DESC'
+				);
+
+			}
+
+			$reports[$interval]['productiveTurns'] = [
+				'title' => 'Turnos mais produtivos',
+				'aliases' => ['Turno', 'Quantidade'],
+				'results' => [],
+			];
+
+			$reports[$interval]['productiveTurns']['results'] = DB::select(
+				'SELECT CASE
+					WHEN EXTRACT(HOUR FROM e.start) >= 0 AND EXTRACT(HOUR FROM e.start) < 6 THEN \'Madrugada\'
+					WHEN EXTRACT(HOUR FROM e.start) >= 6 AND EXTRACT(HOUR FROM e.start) < 12 THEN \'Manhã\'
+					WHEN EXTRACT(HOUR FROM e.start) >= 12 AND EXTRACT(HOUR FROM e.start) < 18 THEN \'Tarde\'
+					ELSE \'Noite\'
+				END AS turn,
+				COUNT(*) AS quantity
+				FROM users u, events e, tasks t
+				WHERE u.id = '.$id.
+				' AND u.id = e.user_id
+				AND e.id = t.event_id
+				AND t.status = \'finished\'
+				AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+				AND e.start <= NOW()
+				GROUP BY turn
+				ORDER BY COUNT(*) DESC'
+			);
+
+			$reports[$interval]['finishedTaskCategories'] = [
+				'title' => 'Categorias de tarefas mais realizadas',
+				'aliases' => ['Categoria', 'Quantidade'],
+				'results' => [],
+			];
+
+			$reports[$interval]['finishedTaskCategories']['results'] = DB::select(
+				'SELECT c.name, COUNT(*) AS quantity
+				FROM events e
+				INNER JOIN categories c ON e.category_id = c.id
+				INNER JOIN tasks t ON t.event_id = e.id
+				WHERE e.user_id = '.$id.
+				' AND e.start >= NOW() - INTERVAL\''.$interval.' DAYS\'
+				AND e.start <= NOW()
+				AND t.status = \'finished\'
+				GROUP BY c.id, c.name
+				ORDER BY quantity DESC'
+			);
+
+			$reports[$interval]['finishedGoalCategories'] = [
+				'title' => 'Categorias de metas mais realizadas',
+				'aliases' => ['Categoria', 'Quantidade'],
+				'results' => [],
+			];
+
+			$reports[$interval]['finishedGoalCategories']['results'] = DB::select(
+				'SELECT c.name,COUNT(*) AS quantity
+				FROM events e
+				INNER JOIN categories c ON e.category_id = c.id
+				INNER JOIN goals g ON g.event_id = e.id
+				WHERE e.user_id = '.$id.
+				' AND e.start >= NOW() - INTERVAL \''.$interval.' DAYS\'
+				AND e.start <= NOW()
+				AND g.status = \'succeeded\'
+				GROUP BY c.id, c.name
+				ORDER BY quantity DESC'
+			);
+		}
 
 		return view('report.index', compact('reports'));
 	}
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -9,18 +9,23 @@
         <div class="flex">
             <h1 class="text-2xl mb-4">
                 Seus relatórios
-            </h1> 
-        </div>
-		@foreach ($reports as $report)
-			<h1 class="text-1xl mb-4 mt-4">
-                {{$report['title']}}
             </h1>
-			<div class="mb-12">
-				@include('components.simple-table', [
-					'header' => $report['aliases'],
-					'content' => $report['results'],
-				])
-			</div>
+        </div>
+		@foreach ($reports as $interval => $reportgroup)
+			<h1 class="text-2xl mb-8 mt-4 b"><b>
+				{{$interval == '7' ? 'Relatórios semanais' : ($interval == '30' ? 'Relatórios mensais' : 'Relatórios anuais')}}
+			</b></h1>
+			@foreach ($reportgroup as $report)
+				<h1 class="text-1xl mb-4 mt-4">
+					{{$report['title']}}
+				</h1>
+				<div class="mb-12">
+					@include('components.simple-table', [
+						'header' => $report['aliases'],
+						'content' => $report['results'],
+					])
+				</div>
+			@endforeach
 		@endforeach
     </div>
 @endsection
